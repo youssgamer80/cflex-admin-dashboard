67408e412709016322033d2383a0aeb08d682720
claude-Dev <claudekme@outlook.fr>
2022-09-14 14:25:21 +0000

    push

--- src/api/api.js
+++ src/api/api.js
@@ -4,13 +4,13 @@ import { getUserData } from '../utility/Utils'
 
 const user = getUserData()
 const client = mockRequests ? axios : user ? axios.create({
-    baseURL: 'http://192.168.252.153:4001/api',
+    baseURL: 'http://192.168.252.23:4001/api',
     headers: {
         "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${user.accesToken}`
     }
 }) : axios.create({
-    baseURL: 'http://192.168.252.153:4001/api',
+    baseURL: 'http://192.168.252.23:4001/api',
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-type": "Application/json"
